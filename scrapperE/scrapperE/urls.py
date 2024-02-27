14490027dc9e4b078e8c74d5f20ad5deaa9a81ee
Zhong_ming_Tan <Zhong_ming_Tan@dell.com>
2024-02-28 00:31:09 +0800

    feat: add login logout register

--- scrapperE/scrapperE/urls.py
+++ scrapperE/scrapperE/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from user import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("scrap/", include("scrap.urls"))
+    path("scrap/", include("scrap.urls")),
+    path('register/',user_views.register, name='register'),
+    path('login/',auth_views.LoginView.as_view(template_name='user/login.html'),name='login'),
+    # path('logout/',auth_views.LogoutView.as_view(template_name='user/logout.html'),name='logout'),
+    path('logout/', user_views.user_logout, name='logout'),
+    path('logout_msg/', user_views.logout_msg, name='logout_msg'),
 ]
