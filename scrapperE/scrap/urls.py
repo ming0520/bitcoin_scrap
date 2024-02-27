34a267c48c9039aeab6bfb8c875bbc265f4086ca
Zhong_ming_Tan <Zhong_ming_Tan@dell.com>
2024-02-27 10:09:24 +0800

    feat: add status to raw and add get_post_by_status

--- scrapperE/scrap/urls.py
+++ scrapperE/scrap/urls.py
@@ -3,8 +3,13 @@ from . import views
 
 urlpatterns = [
     path("all_post/", views.all_post, name="all_post"),
+    path("api/post/post_id/<str:post_id>/", views.get_post_by_id, name="get_post_by_id"),
+    path("api/post/status/<str:status>/", views.get_post_by_status, name="get_post_by_status"),
+
+
     path("scrap_today/", views.scrap_today, name="scrap_today"),
     path("error_post/", views.scrap_today, name="error_post"),
+    path("api/error_log/add/", views.add_error_log, name="add_error_log"),
 
     path("api/bitstamp/add", views.add_bitstamp, name="add_bitstamp"),
     path("api/bitstamphour/add", views.add_bitstamphour, name="add_bitstamphour"),
@@ -13,9 +18,7 @@ urlpatterns = [
     path("api/bitstamp/save_daily", views.save_bitstamp_daily, name="save_bitstamp_daily"),
     path("api/bitstamp/save_hourly", views.save_bitstamp_hourly, name="save_bitstamp_hourly"),
     path("api/bitstamp/save_minutely", views.save_bitstamp_minutely, name="save_bitstamp_minutely"),
-
-    path("api/post/<str:post_id>/", views.get_post_by_id, name="get_post_by_id"),
-    path("api/error_log/add/", views.add_error_log, name="add_error_log"),
+    # path("api/bitstamp/get_daily", views.get_bitstamp_daily, name="get_bitstamp_daily"),    
 
     # Bitstamp Crud
     # path('', views.bitstamp_list, name='bitstamp_list'),
